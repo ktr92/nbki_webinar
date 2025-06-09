fd736afff1fa37bd2f668dde8353713946ba1492
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2025-06-09 15:44:39 +0300

    index

--- js/bundle.js
+++ js/bundle.js
@@ -1,3 +1,146 @@
-/*! For license information please see bundle.js.LICENSE.txt */
-(()=>{var e={r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},t={};$(document).ready((function(){function e(e){e.closest(".mainform__input").removeClass("success").addClass("error"),e.closest(".mainform__input").find(".errortext").show()}function t(e){e.closest(".mainform__input").removeClass("error").addClass("success"),e.closest(".mainform__input").find(".errortext").hide()}$("input[type=tel]").mask("+7 (999) 999-99-99"),$("input.inn").mask("999999999?999"),$("#id_check").on("change",(function(e){$(this).is(":checked")?$("#formsubmit").removeAttr("disabled"):$("#formsubmit").prop("disabled",!0)})),$("form").on("submit",(function(i){i.preventDefault();let o=!0;$("input.required").each((function(){console.log($(this).val()),$(this).val().length<1?(e($(this)),o=!1):t($(this))})),$('input[type="email]"').each((function(){var i;i=$(this),o=/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(i),o?t($(this)):e($(this))})),o&&$("#regmodal").modal("show")}))})),(()=>{"use strict";e.r(t)})()})();
-//# sourceMappingURL=bundle.js.map
+$(document).ready(function () {
+  $("input[type=tel]").mask("+7 (999) 999-99-99")
+  $("input.inn").mask("9999999999")
+checkAgree()
+  function checkAgree() {
+    if ($("#id_check").is(":checked")) {
+      $("#formsubmit").removeAttr("disabled")
+    } else {
+      $("#formsubmit").prop("disabled", true)
+    }
+  }
+
+  $("#id_check").on("change", function (e) {
+    checkAgree()
+  })
+
+  $("a.scrollTo").click(function () {
+    var destination = $($(this).attr("href")).offset().top - 30
+    $("html:not(:animated),body:not(:animated)").animate(
+      {
+        scrollTop: destination,
+      },
+      400
+    )
+    return false
+  })
+
+  function isEmail(email, required = false) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/
+    if (required) {
+      if (email.length < 1) {
+        return false
+      } else {
+        return regex.test(email)
+      }
+    } else {
+      if (email.length > 0) {
+        return regex.test(email)
+      } else {
+        return true
+      }
+    }
+  }
+  function showError(input) {
+    input.closest(".mainform__input").removeClass("success").addClass("error")
+    input.closest(".mainform__input").find(".errortext").show()
+  }
+
+  function hideError(input) {
+    input.closest(".mainform__input").removeClass("error").addClass("success")
+    input.closest(".mainform__input").find(".errortext").hide()
+  }
+
+  $(".inn").suggestions({
+    token: "072fd7f1c87f20233d4c83a1bd5a4cfa53b19b3e",
+    type: "PARTY",
+    onSelect: function (suggestion) {
+      let inn = Number(suggestion.data.inn)
+      $(this).val(inn)
+      $('[name="org"]').val(suggestion.value)
+    },
+  })
+
+  $(".inn").on("change", function () {
+    let val = $(this).val()
+    console.log("w1")
+    console.log("l" + val.length)
+    val = val.replace(/\D/g, "")
+    if (val.length >= 10) {
+      var url =
+        "https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party"
+      var token = "072fd7f1c87f20233d4c83a1bd5a4cfa53b19b3e"
+
+      var options = {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: "Token " + token,
+        },
+        body: JSON.stringify({ query: val }),
+      }
+
+      fetch(url, options)
+        .then((response) => response.text())
+        .then((result) => setInnInfo(result))
+        .catch((error) => console.log("error", error))
+    }
+  })
+
+  function setInnInfo(result) {
+    result = JSON.parse(result)
+    console.log(result.suggestions)
+    if (result.suggestions.length > 0) {
+      hideError($(".inn"))
+      $('[name="org"]').val(result.suggestions[0].value)
+      hideError($('[name="org"]'))
+    } else {
+      showError($(".inn"))
+    }
+  }
+
+  $("form").on("submit", function (e) {
+    e.preventDefault()
+    let valid = true
+    let emailvalid = true
+
+    const $required = $("input.required")
+    const $form = $(this)
+
+    $required.each(function () {
+      console.log($(this).val())
+      if ($(this).val().length < 1) {
+        showError($(this))
+        valid = false
+      } else {
+        hideError($(this))
+      }
+    })
+
+    $("input.email").each(function () {
+      emailvalid = isEmail($(this).val(), $(this).hasClass("required"))
+      if (!emailvalid) {
+        showError($(this))
+      } else {
+        hideError($(this))
+      }
+    })
+
+    if (valid && emailvalid) {
+      $("#u-name").text($('[name="name"]').val())
+      /*  $.post(
+        "/local/ajax/form.landing.php",
+        $form.serialize(),
+        function (result) {
+          console.log("send")
+        }
+      ) */
+      $("#regmodal").modal("show")
+      $("form input").each(function () {
+        $(this).val("")
+      })
+    }
+  })
+})
--- js/index.js
+++ js/index.js
@@ -1,1 +1,145 @@
-$(document).ready((function(){function t(t){t.closest(".mainform__input").removeClass("success").addClass("error"),t.closest(".mainform__input").find(".errortext").show()}function e(t){t.closest(".mainform__input").removeClass("error").addClass("success"),t.closest(".mainform__input").find(".errortext").hide()}$("input[type=tel]").mask("+7 (999) 999-99-99"),$("input.inn").mask("999999999?999"),$("#id_check").on("change",(function(t){$(this).is(":checked")?$("#formsubmit").removeAttr("disabled"):$("#formsubmit").prop("disabled",!0)})),$("form").on("submit",(function(s){s.preventDefault();let i=!0;$("input.required").each((function(){console.log($(this).val()),$(this).val().length<1?(t($(this)),i=!1):e($(this))})),$('input[type="email]"').each((function(){var s;s=$(this),i=/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(s),i?e($(this)):t($(this))})),i&&$("#regmodal").modal("show")}))}));
+$(document).ready(function () {
+  $("input[type=tel]").mask("+7 (999) 999-99-99")
+  $("input.inn").mask("9999999999")
+
+  $("#id_check").on("change", function (e) {
+    if ($(this).is(":checked")) {
+      $("#formsubmit").removeAttr("disabled")
+    } else {
+      $("#formsubmit").prop("disabled", true)
+    }
+  })
+
+  $("a.scrollTo").click(function () {
+    var destination = $($(this).attr("href")).offset().top - 30
+    $("html:not(:animated),body:not(:animated)").animate(
+      {
+        scrollTop: destination,
+      },
+      400
+    )
+    return false
+  })
+
+  function isEmail(email, required = false) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/
+    if (required) {    
+      if (email.length < 1) {
+        return false
+      } else {
+        return regex.test(email)
+      }
+    } else {
+      if (email.length > 0) {
+        return regex.test(email)
+      } else {
+        return true
+      }
+    }
+
+  }
+  function showError(input) {
+    input.closest(".mainform__input").removeClass("success").addClass("error")
+    input.closest(".mainform__input").find(".errortext").show()
+  }
+
+  function hideError(input) {
+    input.closest(".mainform__input").removeClass("error").addClass("success")
+    input.closest(".mainform__input").find(".errortext").hide()
+  }
+
+  $(".inn").suggestions({
+    token: "072fd7f1c87f20233d4c83a1bd5a4cfa53b19b3e",
+    type: "PARTY",
+      onSelect: function(suggestion) {
+        let inn = Number(suggestion.data.inn)
+        $(this).val(inn)
+        $('[name="org"]').val(suggestion.value);
+      }
+    });
+
+  $('.inn').on('change', function () {
+     let val = $(this).val();
+     console.log('w1');
+     console.log('l' + val.length);
+      val = val.replace(/\D/g, "");
+     if (val.length >= 10) {
+       var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party";
+       var token = "072fd7f1c87f20233d4c83a1bd5a4cfa53b19b3e";
+
+       var options = {
+         method: "POST",
+         mode: "cors",
+         headers: {
+           "Content-Type": "application/json",
+           "Accept": "application/json",
+           "Authorization": "Token " + token
+         },
+         body: JSON.stringify({query: val})
+       }
+
+       fetch(url, options)
+           .then(response => response.text())
+           .then(result => setInnInfo(result))
+           .catch(error => console.log("error", error));
+     }
+  })
+
+  function setInnInfo(result) {
+    result = JSON.parse(result);
+    console.log(result.suggestions);
+      if (result.suggestions.length > 0) {
+          hideError($('.inn'));
+          $('[name="org"]').val(result.suggestions[0].value);
+          hideError($('[name="org"]'))
+      } else {
+        showError($('.inn'));
+      }
+  }
+
+
+  $("form").on("submit", function (e) {
+    e.preventDefault()
+    let valid = true
+    let emailvalid = true
+
+    const $required = $("input.required")
+    const $form = $(this)
+
+    $required.each(function () {
+     
+      console.log($(this).val())
+      if ($(this).val().length < 1) {
+        showError($(this))
+        valid = false
+      } else {
+        hideError($(this))
+      }
+    })
+    
+
+    $("input.email").each(function () {
+      emailvalid = isEmail($(this).val(), $(this).hasClass('required'))
+      if (!emailvalid) {
+        showError($(this))
+      } else {
+        hideError($(this))
+      }
+    })
+
+    if (valid && emailvalid) {
+      $("#u-name").text($('[name="name"]').val())
+     /*  $.post(
+        "/local/ajax/form.landing.php",
+        $form.serialize(),
+        function (result) {
+          console.log("send")
+        }
+      ) */
+      $("#regmodal").modal("show")
+      $("form input").each(function () {
+        $(this).val("")
+      })
+    }
+  })
+})
